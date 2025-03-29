726c1d918feb0eedf32bf6eff5cd2596e18072ee
Alden Derf <aldenderfc.fabro99@gmail.com>
2025-03-29 22:07:37 +0800

    implemented the server side logic of the invoice create feature

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -9,7 +9,7 @@ export default async function Page(){
         <main>
             <Breadcrumbs
                 breadcrumbs={[
-                    { label: "Invoices", href: "/invoices" },
+                    { label: "Invoices", href: "/dashboard/invoices" },
                     {
                         label: "Create Invoice",
                         href: "dashboard/invoices/create",
