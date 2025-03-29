6b0b6c740559c049c179767782b099ea61d644e4
Alden Derf <aldenderfc.fabro99@gmail.com>
2025-03-29 23:38:35 +0800

    Update actions.ts
    - added the UpdateInvoice function
    - omited the id and date
    -

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,7 @@ import {z} from 'zod';
 import postgres from 'postgres';
 import {revalidatePath} from 'next/cache';
 import {redirect} from 'next/navigation';
+
 const sql = postgres(process.env.POSTGRES_URL!, {ssl: 'require'});
 
 
@@ -19,6 +20,12 @@ const CreateInvoice = FormSchema.omit({
   id: true,
   date: true
 });
+
+// Use zod to update the expected types
+const UpdateInvoice = FormSchema.omit({
+  id: true, date: true
+});
+
 
 export async function createInvoice(formData: FormData) {
   const {customerId, amount, status }= CreateInvoice.parse({
@@ -37,3 +44,25 @@ export async function createInvoice(formData: FormData) {
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
+
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const {customerId, amount, status} = UpdateInvoice.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get('status')
+  });
+
+  const amountInCents = amount * 100;
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+    `;
+
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+}
+
+
