21a9e5fea58e3192b1c4c26831cd02fa3a065a96
Alden Derf <aldenderfc.fabro99@gmail.com>
2025-09-16 19:54:35 +0800

    added alden page

---
+++ app/alden/page.tsx
@@ -0,0 +1,9 @@
+import Info from "./info";
+export default function Page() {
+    return (
+      <div>
+        <h1 className="text-3xl font-bold">Alden Derf C. Fabro</h1>
+        <Info />
+      </div>
+    );
+}
