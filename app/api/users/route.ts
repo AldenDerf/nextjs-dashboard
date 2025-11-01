5340df4caf4f75202a87e4fc13af3a993490f7ea
Alden Derf <aldenderfc.fabro99@gmail.com>
2025-11-01 18:50:48 +0800

    updating the create user

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,5 +1,7 @@
+import { error } from 'console';
 import {NextResponse} from 'next/server';
 import  {prisma} from '@/app/lib/prisma';
+import bcrypt from "bcryptjs";
 
 // Get: Read all users
 export async function GET() {
@@ -10,13 +12,51 @@ export async function GET() {
 
 // POST Create new user
 export async function POST(req: Request) {
-    const data = await req.json();
-    const user = await prisma.user.create({
-        data: {
-            name: data.name,
-            email:data.email,
-        },
-    })
+   try {
+    const {name, email, password} = await req.json();
 
-    return NextResponse.json(user);
+    // Validate input
+    if (!name || !email || !password) {
+        return NextResponse.json(
+            {error: " Name, email, and password are required"},
+            {status: 400}
+        );
+    }
+
+    // CHeck if email already exist
+    const existingUser = await prisma.user.findUnique({
+        where: { email },
+    });
+
+    if (existingUser) {
+        return NextResponse.json(
+            {error: "Email already exists"},
+            { status: 409 }
+        )
+    }
+
+    // Hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create user in database
+    const newUser = await prisma.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword
+        }
+    });
+
+    // Exclude password from response
+    const { password: _, ...userWithoutPassword } = newUser;
+
+    return NextResponse.json(userWithoutPassword, { status: 201});
+
+   } catch (error: any) {
+    console.error("Error creating user:", error);
+    return NextResponse.json(
+        {error: "Something went wrong while creating user"},
+        {status: 500}
+    );
+   }
 }
