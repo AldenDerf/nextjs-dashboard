7d135844f17a48c9bbfa213adbe69735690e4443
Alden Derf <aldenderfc.fabro99@gmail.com>
2025-11-05 21:48:40 +0800

    added signup, and auth.ts

---
+++ app/test-page/page.tsx
@@ -0,0 +1,14 @@
+import { SignupForm } from "../ui/signup-form";
+const TestPage = () => {
+    return (
+        <div>
+            <p>This is Testpage</p>
+            <br/>
+            <div>
+                <SignupForm />
+            </div>
+        </div>
+    )
+}
+
+export default TestPage;
